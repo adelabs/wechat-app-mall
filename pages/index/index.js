afe30aab0bda099971c474624e2902be882f6dcb
Ade <adereg@163.com>
2018-03-19 00:18:05 +0800

    bypass pages/start/

--- pages/index/index.js
+++ pages/index/index.js
@@ -66,15 +66,11 @@ Page({
     wx.setNavigationBarTitle({
       title: wx.getStorageSync('mallName')
     })
-    /*
-    //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
-      //更新数据
       that.setData({
         userInfo:userInfo
       })
     })
-    */
     wx.request({
       url: 'https://api.it120.cc/' + app.globalData.subDomain + '/banner/list',
       data: {
